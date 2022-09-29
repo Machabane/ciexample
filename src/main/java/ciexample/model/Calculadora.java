82245a50347758502f000686356550641e0a9ed0
Helio Machabane <helio.machabane@gmail.com>
2022-09-29 16:18:41 +0200

    writing a simple test

---
+++ src/main/java/ciexample/model/Calculadora.java
@@ -0,0 +1,8 @@
+package ciexample.model;
+
+public class Calculadora {
+
+	public int somar(int a, int b) {
+		return a + b;
+	}
+}
---
+++ src/test/java/ciexample/CalculadoraTest.java
@@ -0,0 +1,21 @@
+package ciexample;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import ciexample.model.Calculadora;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalculadoraTest {
+
+	@Test
+	public void deveriaSomarDoisNumeroPositivos() { 
+		Calculadora calculadora = new Calculadora();
+		int soma = calculadora.somar(3, 7);
+		
+		assertEquals(11, soma); 
+	}
+}
